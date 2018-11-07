1fb60d39f16608562139fbdce2ae42f43463fcc5
Carlos Pradenas <carlpradenas@gmail.com>
2018-11-07 02:36:02 -0300

    Actualizacion de Nombre a Staging para vista de logs y Stack de errores

--- Doitclick/Startup.cs
+++ Doitclick/Startup.cs
@@ -76,7 +76,7 @@ namespace Doitclick
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ApplicationDbContext context)
         {
-            if (env.IsDevelopment())
+            if (env.IsDevelopment() || env.IsStaging())
             {
                 app.UseDeveloperExceptionPage();
             }
