65dee8bcb3d68204ac6d15a818e046c3883449fa
Cristian <cmunozdonos@technisys.com>
2019-08-15 22:06:51 -0400

    vista laboratorista, se agrega el js de vue.

--- Doitclick/Controllers/ListadoLaboratorioController.cs
+++ Doitclick/Controllers/ListadoLaboratorioController.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Doitclick.Models.Application;
 using Microsoft.EntityFrameworkCore;
+using Doitclick.Data.Repository;
 
 namespace Doitclick.Controllers
 {
@@ -15,14 +16,38 @@ namespace Doitclick.Controllers
     public class ListadoLaboratorioController : Controller
     {
          private readonly ApplicationDbContext _context;
-        public ListadoLaboratorioController(ApplicationDbContext context)
+         private readonly ILaboratorioRepository _list; 
+        public ListadoLaboratorioController(ApplicationDbContext context, ILaboratorioRepository list )
         {
             _context = context;
+            _list = list;
         }
           public IActionResult Listado()
         {
             return View();
         }
+        
+        public IEnumerable<IDataResult> Data()
+        {
+           
+            
+            return _list.Data();
+        }
        
     }
+
+    public class IDataResult
+    {
+        public string rut { get; set; }
+        public string nombre { get; set; }
+        public IList<ITarea> tareas { get; set; }
+    }
+    public class ITarea
+    {
+        public string id { get; set; }
+         public string descr { get; set; }
+         public string estado { get; set; }
+
+    }
+    
 }
